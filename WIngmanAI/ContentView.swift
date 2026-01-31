f158e6d8de466f891c3fe262319268753e956656
nyko073006 <niklashumplik@gmail.com>
2026-01-31 20:06:09 +0100

    Initial Commit

---
+++ WIngmanAI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WIngmanAI
+//
+//  Created by Nyko on 31.01.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WIngmanAI/WIngmanAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WIngmanAIApp.swift
+//  WIngmanAI
+//
+//  Created by Nyko on 31.01.26.
+//
+
+import SwiftUI
+
+@main
+struct WIngmanAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
